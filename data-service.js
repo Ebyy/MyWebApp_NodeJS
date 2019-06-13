b867cd5bd5955e9993ba218b222999a4aa891a62
Ebyy <eberechi_oo@yahoo.com>
2019-06-13 17:57:53 -0400

    assign 3 - filters and ways to add data done

--- data-service.js
+++ data-service.js
@@ -1,3 +1,5 @@
+//import { stat } from "fs";
+
 const fs = require("fs");
 var employees=[];
 var departments =[];
@@ -7,36 +9,28 @@ module.exports.initialize = function(){
     return new Promise(function(resolve,reject){
         fs.readFile('./data/employees.json', 'utf8', (err,data)=>{
             if(err){
-                throw err;               
+                throw err;
+                reject("No data found");
+                return;              
             }
             
-             if(data.length != 0){
-                 employees = JSON.parse(data);
-                 resolve();
-                 return;
-             } 
-             else{
-                reject("No Employee data found");
-                return;
-            }
-        })
-    
-    
-       fs.readFile('./data/departments.json', 'utf8', (err,data)=>{
-        if(err){  
-            throw err;               
-        }
-        
-         if(data.length !== 0){
-             departments = JSON.parse(data);
-             resolve();
-             return;
-         } 
-         else{
-            reject("No Department data found");
-            return;
-        }
-        })
+            if(data.length != 0){
+                employees = JSON.parse(data);
+                fs.readFile('./data/departments.json', 'utf8', (err,data)=>{
+                    if(err){  
+                        throw err; 
+                        reject("No data found");
+                        return;            
+                    }
+                    
+                    if(data.length !== 0){
+                        departments = JSON.parse(data);
+                        resolve();
+                        return;
+                    } 
+                })
+            } 
+        })       
     })
 
 }
@@ -82,4 +76,100 @@ module.exports.getManagers = function(){
         }
         
     })    
+}
+
+module.exports.addEmployee = function(empData){
+    return new Promise(function(resolve,reject){
+        if(empData.isManager !== true){
+            empData.isManager = false;
+        } 
+        empData.emloyeeNum = employees.length + 1;
+        employees.push(empData);
+        resolve();
+        
+    })   
+}
+
+//call that returns employees by status
+module.exports.getEmployeesByStatus = function(status){
+    return new Promise(function(resolve,reject){
+        if(employees.length > 0){
+            var byStatus = [];
+            for(let i = 0; i < employees.length; i++){
+                if(employees[i].status == status){
+                    byStatus.push(employees[i]);
+                }
+            }
+            if(byStatus.length == 0){
+                reject("no by status data");
+                return;
+            }else{
+                resolve(byStatus);
+                console.log("resolved by status");
+            }
+        }
+        else{
+            reject("no employees data");
+        }
+    })    
+}
+
+// call that filters employees by their department
+module.exports.getEmployeesByDepartment = function(department){
+    return new Promise(function(resolve,reject){
+        if(employees.length > 0){
+            var byDept = [];
+            for(let i=0; i < employees.length; i++){
+                if(employees[i].department == department){
+                    byDept.push(employees[i]);
+                    //console.log("pushed to by dept");
+                }
+            }
+            if(byDept.length == 0){
+                reject("no by dept data");
+                return;
+            }else{
+                resolve(byDept);
+                console.log("resolved by dept");
+            }
+        }
+        else{
+            reject("no employees data");
+        }
+
+    })
+}
+
+// filters employees by that have the same manager
+module.exports.getEmployeesByManager = function(manager){
+    return new Promise(function(resolve,reject){
+        if(employees.length > 0){
+            let byManager = [];
+            byManager = employees.filter(employees=>employees.employeeNum == manager);
+            if(byManager.length == 0){
+                reject("no by manger data");
+                return;
+            }
+            resolve(byManager);
+        }else{
+            reject("no employees data");
+        }
+    })
+}
+//filters a particular employee with the employee number parsed
+module.exports.getEmployeeByNum = function(num){   
+    return new Promise(function(resolve,reject){
+        if(employees.length > 0){
+            let empObj = [];
+            empObj = employees.filter(employees=>employees.employeeNum == num);
+            if(empObj.length == 0){
+                reject("no emp data retrieved");
+            }
+            resolve(empObj);
+        }
+        else{
+            reject("no employees data");
+        }
+
+    })
 }
--- server.js
+++ server.js
@@ -11,6 +11,9 @@
 
 var data = require("./data-service.js");
 var express = require("express");
+var multer = require("multer");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 var app = express();
 var path = require("path");
 
@@ -24,6 +27,22 @@ function onHttpStart() {
 // function call that connects css folder public
 app.use(express.static('public')); 
 
+//set middleware for urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// defining storage variable with multer
+const storage = multer.diskStorage({
+    destination: "./public/images/uploaded",
+    filename: function (req, file, cb) {
+
+      cb(null, Date.now() + path.extname(file.originalname));
+    }
+  });
+  
+  // tell multer to use the diskStorage function for naming files instead of the default.
+  const upload = multer({ storage: storage });
+
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res){
     res.sendFile(path.join(__dirname,"/views/home.html"));
@@ -36,14 +55,44 @@ app.get("/about", function(req,res){
 
 // route for employees
 app.get("/employees", function(req,res){
-    data.getAllEmployees()
-    .then(function(data){
-        res.json(data);
-    }        
-    )
-    .catch(function(err){
-        res.json({message: err});
-    })
+
+    if(req.query.status){
+        data.getEmployeesByStatus(req.query.status)
+        .then(function(data){
+            res.json(data);
+        }).catch(function(err){
+            res.json({message: err});
+        })
+    }
+
+    else if(req.query.department){
+        data.getEmployeesByDepartment(req.query.department)
+        .then(function(data){
+            res.json(data);
+        }).catch(function(err){
+            res.json({message: err});
+        })
+    }
+
+    else if(req.query.manager){
+        data.getEmployeesByManager(req.query.manager)
+        .then(function(data){
+            res.json(data);
+        }).catch(function(err){
+            res.json({message: err});
+        })
+    }
+    else{
+        data.getAllEmployees()
+        .then(function(data){
+            res.json(data);
+        }        
+        )
+        .catch(function(err){
+            res.json({message: err});
+        })
+    }
+
 });
 
 // route for departments
@@ -70,6 +119,46 @@ app.get("/managers", function(req,res){
     })
 });
 
+// route that sends the file "/views/addEmployee.html "
+app.get("/employees/add", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/addEmployee.html"));
+});
+
+// route that sends the file "/views/addImage.html "
+app.get("/images/add", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/addImage.html"));
+});
+
+// route that sends file "/views/addImage.html " with the POST method
+app.post("/images/add", upload.single("imageFile"), (req,res) => {
+    res.redirect("/images");
+});
+
+//route to get uploaded image data 
+app.get("/images", function(req,res){
+    fs.readdir('./public/images/uploaded', (err,data)=>{
+        res.json({image:data});
+    });
+});
+
+// route that sends file "/views/addEmployee " with the POST method
+app.post("/employees/add", (req,res) => {
+    data.addEmployee(req.body)
+    .then(function(){
+        res.redirect("/employees");
+    }        
+    )
+});
+
+
+//route for "employee/value"
+app.get("/employee/:value", function(req,res){
+    console.log("hey");
+    data.getEmployeeByNum(req.params.value)
+    .then(function(data){
+        res.json(data);
+    })
+})
 
 // setup http server to listen on HTTP_PORT , loads up all the data as well
 data.initialize().then (app.listen(HTTP_PORT, onHttpStart))
